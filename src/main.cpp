adec256c647b930c4090183ef3973a37222c551b
Chris Brady <15155129@studentmail.ul.ie>
2018-10-12 12:56:51 +0100

    Added GLAD as normal folder, downgrade to GL version 4.5 <- 4.6

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(void) {
     //Initialize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Voxagen", NULL, NULL);
